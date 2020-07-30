0206aa8bc2876fe79e80bea7a7f9bd6d93d0fbf1
Bobby Papson <32421699+BobbyPapson@users.noreply.github.com>
2020-07-30 19:21:14 -0400

    Add files

---
+++ AccuWeather App/weather_app/scripts/forecast.js
@@ -0,0 +1,35 @@
+const key = 'b1B7VUr0HfJ8VbIqmHG1TFUIjM7dDj6M';
+
+// GET WEATHER INFORMATION
+
+const getWeather = async(idKey) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${idKey}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return (data[0]);
+
+};
+
+// GET CITY INFORMATION
+const getCity = async(city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return (data[0]);
+};
+
+/* EXAMPLE CODE FOR TEXT
+getCity('atlanta').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err));
+*/
